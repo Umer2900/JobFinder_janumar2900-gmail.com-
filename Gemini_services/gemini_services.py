7c0d498bd4615b74e255eda77f5f021731c8d6a5
Umer <janumar2900@gmail.com>
2025-09-26 17:43:40 +0530

    DONE

--- Gemini_services/gemini_services.py
+++ Gemini_services/gemini_services.py
@@ -10,7 +10,8 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 # Configure Gemini AI
 genai.configure(api_key=GEMINI_API_KEY)
-model = genai.GenerativeModel(model_name="gemini-1.5-flash")
+# model = genai.GenerativeModel(model_name="gemini-1.5-flash")   # OLD
+model = genai.GenerativeModel(model_name="gemini-1.5-flash-latest")
 
 
 
